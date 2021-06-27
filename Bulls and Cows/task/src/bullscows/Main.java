2066bb6cf8c722db816e8487b5a51348e86c8658
jetonramadani <jeton.ramadani@students.finki.ukim.mk>
2021-06-27 20:51:22 +0200

    Initial commit

---
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -0,0 +1,9 @@
+package bullscows;
+class BullAndCows {
+    private String num;
+}
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
